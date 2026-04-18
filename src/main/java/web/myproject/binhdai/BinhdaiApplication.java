25772ddcab8335155e3d82dd6f9bc76707455a28
Nguyễn Văn Thiện <quocthien19950404@gmail.com>
2026-04-18 20:57:18 +0700

    File goc chua co cai gi

---
+++ src/main/java/web/myproject/binhdai/BinhdaiApplication.java
@@ -0,0 +1,13 @@
+package web.myproject.binhdai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinhdaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinhdaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/myproject/binhdai/BinhdaiApplicationTests.java
@@ -0,0 +1,13 @@
+package web.myproject.binhdai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinhdaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
