cf98b6f1ad18faa6703033279ff7f376ac6b9676
Nguyễn Văn Thiện <quocthien19950404@gmail.com>
2026-04-20 13:43:55 +0700

    Thêm file home.html để test thử

--- src/main/java/web/myproject/binhdai/BinhdaiApplication.java
+++ src/main/java/web/myproject/binhdai/BinhdaiApplication.java
@@ -2,8 +2,16 @@ package web.myproject.binhdai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
+// Bởi du án còn đang thử nghiệm nên có các thông tin như dưới đây
+// Một khi đã co dự án đầy đủ thì không cần nữa
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class
+})
 public class BinhdaiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/web/myproject/binhdai/controller/MyController.java
@@ -0,0 +1,14 @@
+package web.myproject.binhdai.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    public String showHomePage(Model model) {
+        return "home";
+    }
+}
